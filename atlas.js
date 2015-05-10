2ae503c4c8172dd76c7d85e8e155bb0d7b4378ee
laixintao <laixintao1995@163.com>
2015-05-11 02:12:26 +0800

    添加被动道具，完成，可查询

--- atlas.js
+++ atlas.js
@@ -10,16 +10,11 @@ function creat_active_tools_atlas(id,type,en_name,ch_name,charge_time,effect){
 	return active_tools_atlas;
 }
 
-function get_text(o){
-	var str = o.id+" "+o.type+" "+o.en_name+" "+o.ch_name+" "+charge_time+" "+effect;
-	return str;
-}
-
 function get_active_tools_atlas(){
 	var all_atlas = new Array();
 
 	for(var i=0;i<73;i++){
-		var temp = "atlas#at"+i;
+		var temp = "atlas.active_tools#at"+i;
 		var id = i;
 		var ch_name = $(temp+" > ch_name").text();
 		var type = $(temp+" > type").text();
@@ -27,6 +22,34 @@ function get_active_tools_atlas(){
 		var charge_time = $(temp+" > charge_time").text();
 		var effect = $(temp+" > effect").text();
 		all_atlas.push(creat_active_tools_atlas(id,type,en_name,ch_name,charge_time,effect));
+	}
+	return all_atlas;
+}
+
+
+
+function creat_passive_tools_atlas(id,type,en_name,ch_name,effect){
+	var active_tools_atlas = new Object();
+	active_tools_atlas.id = id;
+	active_tools_atlas.type=type;
+	active_tools_atlas.en_name=en_name;
+	active_tools_atlas.ch_name=ch_name;
+	active_tools_atlas.effect=effect;
+	return active_tools_atlas;
+}
+
+
+function get_passive_tools_atlas(){
+	var all_atlas = new Array();
+
+	for(var i=0;i<184;i++){
+		var temp = "atlas.passive_tools#at"+i;
+		var id = i;
+		var ch_name = $(temp+" > ch_name").text();
+		var type = $(temp+" > type").text();
+		var en_name = $(temp+" > en_name").text();
+		var effect = $(temp+" > effect").text();
+		all_atlas.push(creat_passive_tools_atlas(id,type,en_name,ch_name,effect));
 	}
 	return all_atlas;
 }
--- main.js
+++ main.js
@@ -1,10 +1,12 @@
 var active_tools;
+var passive_tools;
 
 $().ready(function(){
 	
 	active_tools = get_active_tools_atlas();
+	passive_tools = get_passive_tools_atlas();
 	display_active_tools(active_tools);
-
+	display_passive_tools(passive_tools);
 });
 
 //展示主动道具的图片
@@ -16,7 +18,7 @@ function display_active_tools(all){
 };
 
 //设置监听，显示查询的主动道具的内容
-$(document).delegate(".active_tools","click",function(){
+$(document).delegate("img.active_tools","click",function(){
 	var click_id=this.id;
 	click_id = click_id.replace("img","");
 
@@ -24,6 +26,28 @@ $(document).delegate(".active_tools","click",function(){
 	"英文名："+active_tools[click_id].en_name+"<br />"
 	+"充能时间："+active_tools[click_id].charge_time+"<br />"+
 	"效果："+active_tools[click_id].effect+"</p>";
+	
+	$("#display > p").detach();  //先移除之前的元素，在添加新的
+	$("#display").append(str);
+});
+
+//展示被动道具的图片
+function display_passive_tools(all){
+	for(var i=0;i<all.length;i++){
+		var photo = "<img class='passive_tools'  id='img"+all[i].id+"' src='graphics/passive_tools/"+(all[i].id+1)+".png' />";
+		$("#passive_tools").append(photo);
+	}
+};
+
+//设置监听，显示查询的主动道具的内容
+$(document).delegate("img.passive_tools","click",function(){
+	var click_id=this.id;
+	click_id = click_id.replace("img","");
+
+	var str = "<p>中文名："+passive_tools[click_id].ch_name+"<br />"+
+	"英文名："+passive_tools[click_id].en_name+"<br />"
+	+
+	"效果："+passive_tools[click_id].effect+"</p>";
 	
 	$("#display > p").detach();  //先移除之前的元素，在添加新的
 	$("#display").append(str);
