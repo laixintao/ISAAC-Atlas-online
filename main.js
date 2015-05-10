6a15f50d0d39778ec92beb7713dffe30c2bbcb60
laixintao <laixintao1995@163.com>
2015-05-11 01:08:44 +0800

    里程碑---alpha版本完成
    这是一个最简陋的版本，基本的查询功能已经可以正常工作了。

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
+var active_tools;
+
 $().ready(function(){
 	
-	var active_tools = get_active_tools_atlas();
+	active_tools = get_active_tools_atlas();
 	display_photo(active_tools);
 
 });
@@ -11,3 +13,16 @@ function display_photo(all){
 		$("#active").append(photo);
 	}
 };
+
+$(document).delegate(".active_tools","click",function(){
+	var click_id=this.id;
+	click_id = click_id.replace("img","");
+
+	var str = "<p>中文名："+active_tools[click_id].ch_name+"<br />"+
+	"英文名："+active_tools[click_id].en_name+"<br />"
+	+"充能时间："+active_tools[click_id].charge_time+"<br />"+
+	"效果："+active_tools[click_id].effect+"</p>";
+	
+	$("#display > p").detach();  //先移除之前的元素，在添加新的
+	$("#display").append(str);
+});
