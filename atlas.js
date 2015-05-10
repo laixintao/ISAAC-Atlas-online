d70764c0501c0ef45bcfefe2421bb23e0de5bc77
laixintao <laixintao1995@163.com>
2015-05-10 13:35:55 +0800

    不采用Dom的方式解析了，直接采用元素的id来识别，已经能成功读取名字
    Dom解析遇到的困难太大了，直接采用元素的id来识别吧。

--- atlas.js
+++ atlas.js
@@ -10,18 +10,42 @@ function creat_active_tools_atlas(id,type,en_name,ch_name,charge_time,effect){
 	return active_tools_atlas;
 }
 
-function get_active_tools_atlas(){
-	$("atlas").each(function(){
-		
-		var co = $(this).contents();
-		id=co.add("id").text();
-		type=co.add("type").text;
-		en_name=co.add("en_name").text();
-		ch_name=co.add("ch_name").text();
-		charge_time=co.add("charge_time").text();
-		effect=co.add("effect").text();
+function get_text(o){
+	var str = o.id+" "+o.type+" "+o.en_name+" "+o.ch_name+" "+charge_time+" "+effect;
+	return str;
+}
 
-		var o = creat_active_tools_atlas(id,type,en_name,ch_name,charge_time,effect)
-		console.log(o);
-	});
+function get_active_tools_atlas(){
+	var all_atlas = new Array();
+	// $("atlas").each(function(){
+	// 	all_atlas.push(this.contents().text());
+	// });
+
+	// //console.log(all_atlas);
+
+	// for(var i in all_atlas[0]){
+	// 	if(all_atlas[0].hasOwnProperty(i))
+	// 		console.log(i);
+	// }
+
+	// for(var i in all_atlas){
+	// 	var co = i.contents();
+	// 	id=co.add("id").text();
+	// 	type=co.add("type").text();
+	// 	en_name=co.add("en_name").text();
+	// 	ch_name=co.add("ch_name").text();
+	// 	charge_time=co.add("charge_time").text();
+	// 	effect=co.add("effect").text();
+
+	// 	var o = creat_active_tools_atlas(id,type,en_name,ch_name,charge_time,effect);
+	// 	var p =counter+" "+ get_text(o);
+	// 	console.log(p);
+	// };
+
+	for(var i=0;i<73;i++){
+		var temp = "atlas#at"+i;
+		var str = $(temp+" > ch_name").text();
+		console.log(i+" "+str);
+	}
+	return all_atlas;
 }
