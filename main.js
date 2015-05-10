7ffac18e44b1bae903eeaca3c856aeedc60a8994
laixintao <laixintao1995@163.com>
2015-05-11 00:13:50 +0800

    显示图片 （存在一个遍历问题
    使用对象数组，遍历的时候只能用下表吗？ 用in循环，显示的不是对象呀？

--- main.js
+++ main.js
@@ -1,6 +1,22 @@
 $().ready(function(){
 	
 	var active_tools = get_active_tools_atlas();
-
+	display_photo(active_tools);
 });
+
+function display_photo(all){
+	for(var i=0;i<all.length;i++){
+		console.log(all[i]);
+	}
+
+
+	for(var i in all){
+		console.log(i);
+		//console.log(i.id+i.en_name);
+		var photo = "<img src='graphics/active_tools/"+i+".png' />";
+		//console.log(photo);
+		$("#active").append(photo);
+	}
+}
+
 
