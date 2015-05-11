9f7a8ed219455f475ede81654df7352958f05f29
laixintao <799303812@163.com>
2015-05-11 16:11:59 +0800

    更新 readme

--- main.js
+++ main.js
@@ -41,32 +41,4 @@ function on_Click_Listener(myclick){
 }
 
 
-// //设置监听，显示查询的主动道具的内容
-// $(document).delegate("img.active_tools","click",function(){
-// 	var click_id=this.id;
-// 	click_id = click_id.replace("img","");
-
-// 	var str = "<p>中文名："+active_tools[click_id].ch_name+"<br />"+
-// 	"英文名："+active_tools[click_id].en_name+"<br />"
-// 	+"充能时间："+active_tools[click_id].charge_time+"<br />"+
-// 	"效果："+active_tools[click_id].effect+"</p>";
-	
-// 	$("#display > p").detach();  //先移除之前的元素，在添加新的
-// 	$("#display").append(str);
-// });
-
-
-// //设置监听，显示查询的主动道具的内容
-// $(document).delegate("img.passive_tools","click",function(){
-// 	var click_id=this.id;
-// 	click_id = click_id.replace("img","");
-
-// 	var str = "<p>中文名："+passive_tools[click_id].ch_name+"<br />"+
-// 	"英文名："+passive_tools[click_id].en_name+"<br />"
-// 	+
-// 	"效果："+passive_tools[click_id].effect+"</p>";
-	
-// 	$("#display > p").detach();  //先移除之前的元素，在添加新的
-// 	$("#display").append(str);
-// });
 
