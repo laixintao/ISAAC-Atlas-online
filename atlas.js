b8f1d17b0d464a39e9571545283e15123f4c8cff
laixintao <laixintao1995@163.com>
2015-05-14 02:13:22 +0800

    添加塔罗牌

--- atlas.js
+++ atlas.js
@@ -2,6 +2,7 @@
 
 var num_active_tools = 75;
 var num_Accessories = 267;
+var num_Card = 32;
 
 //**************************************
 
@@ -60,6 +61,35 @@ function get_Accessories(){
 		var effect = $(temp+" > effect").text();
 		var unlock = $(temp+" > unlock").text();
 		all_atlas.push(creat_Accessories(id,type,en_name,ch_name,effect,unlock));
+	}
+	return all_atlas;
+}
+
+
+function creat_Cards(id,type,en_name,ch_name,effect,unlock){
+	var active_tools_atlas = new Object();
+	active_tools_atlas.id = id;
+	active_tools_atlas.atype=type;
+	active_tools_atlas.en_name=en_name;
+	active_tools_atlas.ch_name=ch_name;
+	active_tools_atlas.effect=effect;
+	active_tools_atlas.unlock=unlock;
+	return active_tools_atlas;
+}
+
+
+function get_Cards(){
+	var all_atlas = new Array();
+
+	for(var i=0;i<num_Accessories;i++){
+		var temp = "atlas.card#at"+i;
+		var id = i;
+		var ch_name = $(temp+" > ch_name").text();
+		var type = $(temp+" > type").text();
+		var en_name = $(temp+" > en_name").text();
+		var effect = $(temp+" > effect").text();
+		var unlock = $(temp+" > unlock").text();
+		all_atlas.push(creat_Cards(id,type,en_name,ch_name,effect,unlock));
 	}
 	return all_atlas;
 }
--- main.js
+++ main.js
@@ -4,6 +4,7 @@ var Accessories;
 $().ready(function(){
 	active_tools = get_active_tools_atlas();
 	Accessories = get_Accessories();
+	cards = get_Cards();
 	add_Listener();
 });
 
@@ -15,7 +16,9 @@ function add_Listener(){
 	$("#Accessories > img").click(function(){
 	on_Click_Listener(this);
 	});
-
+	$("#cards > img").click(function(){
+	on_Click_Listener(this);
+	});
 	$("#close > img").click(function(){
 		$("#board").css("display","none");
 	});
@@ -51,6 +54,17 @@ function on_Click_Listener(myclick){
 		"<div class='temp' style='color:#8DB2C9;clear:both;'><span style='background-color: #FFD700;'><strong>效果：</strong></span>"+Accessories[click_id].effect;
 		if(Accessories[click_id].unlock.length>0)
 			str+="<br /><span style='background-color: #FFD700;'><strong>解锁方式：</strong></span>"+Accessories[click_id].unlock;
+		str+="</p><div>";
+	}else if(this_class==="cards"){
+		str = "<div class='temp' style='float:left;'><img style='max-height: 50px;max-width: 50px;width: 50px;margin: 5px;' src='graphics/cards/"+
+		num+".jpg'></div><div class='temp' style='font-size:18px;float:left;'><p ><strong>"+
+
+		cards[click_id].ch_name+"("+cards[click_id].atype+")</strong><br />"+
+		"<i>"+cards[click_id].en_name+"</i><br /></p></div>"+
+
+		"<div class='temp' style='color:#8DB2C9;clear:both;'><span style='background-color: #FFD700;'><strong>效果：</strong></span>"+cards[click_id].effect;
+		if(cards[click_id].unlock.length>0)
+			str+="<br /><span style='background-color: #FFD700;'><strong>解锁方式：</strong></span>"+cards[click_id].unlock;
 		str+="</p><div>";
 	}
 	
